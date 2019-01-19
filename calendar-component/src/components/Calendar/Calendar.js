6f902d2d230a6d3e1fa2f2e6fe74fc8b84b5a6e6
unknown <donvasilyok@gmail.com>
2019-01-19 14:09:24 +0200

    Correct change title

--- calendar-component/src/components/Calendar/Calendar.js
+++ calendar-component/src/components/Calendar/Calendar.js
@@ -9,8 +9,8 @@ let month =moment(moment().month()).format("MMM");
 let nextMonth =moment(moment().month()).format("MMM");
 let firstDay =  moment(moment().day(1).week(week)).format("D");
 let lastDay =  moment(moment().day(0).week(week+1)).format("D");
+let curDay =moment( moment()).format("D");
 let index =1;
-let indexMonth = 0;
 
 class Calendar extends Component {
     constructor(props) {
@@ -20,22 +20,21 @@ class Calendar extends Component {
         this.state ={
             dayWeekName: ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],//["Пн","Вт","Ср","Чт","Пт","Сб","Вс"],
             curWeek: week,     
-            curMonth: month,     
+            curMonth: month, 
+            curDate:  curDay,   
             firstDayWeek: moment(moment().day(1).week(week)).format("D")
         };
        
     }
 
     render() {
-        console.log("curWeek >>> ",`${this.state.curWeek}`);
-        console.log("firstDayWeek >>> ",`${this.state.firstDayWeek}`);
+     
         return (
             <>
                 <div className={"mainContainer"}>
                     <div className={"header"}>
                         <div className={"prev"} onClick={() => this.onPrevNextClick("-")}>PREV</div>
-                        {this.renderTitle()}
-                       
+                        {this.renderTitle()}                       
                         <div className={"next"} onClick={() => this.onPrevNextClick("+")}>NEXT</div>
                     </div>
 
@@ -66,24 +65,29 @@ class Calendar extends Component {
         );
     }
 
-    onPrevNextClick(symbol) {
-        this.setState({curWeek: symbol === "+" ? this.state.curWeek + 1 : this.state.curWeek - 1});
-       if( symbol === "+" ){week += 1;} else{ week -= 1};
-       console.log("week >>> ",week);
+    onPrevNextClick(symbol) {      
+        this.setState({curWeek: symbol === "+" ? (this.state.curWeek + 1, week += 1) : (this.state.curWeek - 1, week -= 1)});
+    
         this.setState({ firstDayWeek: moment(moment().day("Monday").week(week)).format("D")});
-        
+        firstDay =  moment(moment().day(1).week(week)).format("D");
+        lastDay =  moment(moment().day(0).week(week+1)).format("D");
+      
+        if(Number.parseInt(firstDay) > Number.parseInt(lastDay) ){          
+            month =moment(moment().week(week-1)).format("MMM");
+            nextMonth =moment(moment().week(week)).format("MMM");
+        }  else{
+            month =moment(moment().week(week)).format("MMM");
+            nextMonth =moment(moment().week(week+1)).format("MMM");
+        }
+      
         numberDayWeek =-1;
         numberDateWeek = 0;
-        index = 1;
-        if(Number.parseInt(firstDay) > Number.parseInt(lastDay)){month = nextMonth;}
-       
+        index = 1;       
     }
-    renderDayWeekName() {
-        
-       
-       if(numberDayWeek === 6){ numberDayWeek = 0; }
-        else{ numberDayWeek += 1;};
-        console.log("numberDayWeek >>> ",numberDayWeek);
+
+    renderDayWeekName() { 
+       if(numberDayWeek === 6) { numberDayWeek = 0; }
+       else{ numberDayWeek += 1; };       
        
         return   <div className={"dayOfWeek"}
                       style={{"color":"#FFFFFF",
@@ -96,53 +100,68 @@ class Calendar extends Component {
     };
 
     renderDateWeek() {      
-
-        
-        console.log("index >>> ",index);
         if(index === 7){ numberDateWeek = moment(moment().day(0).week(week+1)).format("D");}
         else{numberDateWeek = moment(moment().day(index).week(week)).format("D");}
 
-        index++;
-      
-
-        console.log("numberDateWeek >>> ",numberDateWeek);
+        index++;       
        
+       if(Number.parseInt(numberDateWeek) === Number.parseInt(curDay)){
         return   <div className={"dayOfWeek"}
-                      style={{"color":"#0B3157",
+        style={{display: "flex",
+            "alignItems":"center",
+            "justifyContent":"center",
+            "color":"#FFFFFF",
+            "background":"#0B3157",
+            "borderRadius":"5px",
+            "fontFamily":"Montserrat",
+            "fontSize":"16px",
+            "fontWeight":"400",
+            "letterSpacing":"-0.28px",
+            "width":"40px",
+            "height":"40px",
+            "textAlign":"center"
+            }}
+>{numberDateWeek} </div>;
+       }else{
+        return   <div className={"dayOfWeek"}
+             onClick={() => this.onDateClick()}
+             //text-content = "numberDateWeek"
+                      style={{ display: "flex",
+                          "alignItems":"center",
+                          "justifyContent":"center",
+                          "color":"#0B3157",
+                          "background":"#FFFFFF",
+                          "borderRadius":"5px",
                           "fontFamily":"Montserrat",
                           "fontSize":"16px",
                           "fontWeight":"400",
                           "letterSpacing":"-0.28px",
                           "width":"40px",
                           "height":"40px",
-                          "textAlign":"center"}}
+                          "textAlign":"center"
+                         }}
         >{numberDateWeek} </div>;
-    };
-     renderTitle() {
-        console.log("renderTitle");
-        firstDay =  moment(moment().day(1).week(week)).format("D");
-        lastDay =  moment(moment().day(0).week(week+1)).format("D");
-        // nextMonth =moment(moment().add(1,'months')).format("MMM");
-    
-        console.log("firstDay >>> ",firstDay);
-        console.log("lastDay >>>",lastDay);
-        //console.log("nextMonths >>> ",nextMonth);
-    if(Number.parseInt(firstDay) > Number.parseInt(lastDay)){
-    indexMonth += 1;
-    nextMonth =moment(moment().add(indexMonth,'months')).format("MMM");
-    console.log("nextMonths >>> ",nextMonth);
-    return <div className={"currentWeek"}>
-    {`${month} ${firstDay} - ${nextMonth} ${lastDay}`}
-    </div>
-    
-    
-}else{
-    return <div className={"currentWeek"}>
-    {`${month} ${firstDay} - ${lastDay}`}
-    </div>
-}
+    }
     };
 
+     renderTitle() {        
+       
+      
+    if(Number.parseInt(firstDay) > Number.parseInt(lastDay)){  
+       return <div className={"currentWeek"}>
+       {`${month} ${firstDay} - ${nextMonth} ${lastDay}`}
+       </div>        
+    }else{
+       return <div className={"currentWeek"}>
+       {`${month} ${firstDay} - ${lastDay}`}
+       </div>
+    }
+    };
+ onDateClick() {    
+     console.log("onDateClick");
+    curDay= this.props.content;
+    this.setState({curDay: this.state.curDate});
+ }
 }
 
 export  default  Calendar;
